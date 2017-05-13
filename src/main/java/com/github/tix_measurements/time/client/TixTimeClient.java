1b91b11b2afebdc7752d68679048002ca6fc8625
jperezcu <jperezcu@live.com>
2017-05-13 11:03:40 -0300

    Fixed file formatting and data packet creation

--- src/main/java/com/github/tix_measurements/time/client/TixTimeClient.java
+++ src/main/java/com/github/tix_measurements/time/client/TixTimeClient.java
@@ -35,171 +35,184 @@ import java.util.concurrent.Executors;
 
 public class TixTimeClient {
 
-	private static final Logger logger = LogManager.getLogger();
-	private static final Timer timer = new Timer();
-	private static final KeyPair keyPair = TixCoreUtils.NEW_KEY_PAIR.get();
+    private static final Logger logger = LogManager.getLogger();
+    private static final Timer timer = new Timer();
 
-	public static final int WORKER_THREADS;
-	public static final int DEFAULT_CLIENT_PORT;
-	public static final int DEFAULT_SERVER_PORT;
-	public static final InetSocketAddress DEFAULT_SERVER_ADDRESS;
-	public static final int LONG_PACKET_MAX_RETRIES; /* how many times will payload with measurement data be sent after every minute */
+    public static final int WORKER_THREADS;
+    public static final int DEFAULT_CLIENT_PORT;
+    public static final int DEFAULT_SERVER_PORT;
+    public static final InetSocketAddress DEFAULT_SERVER_ADDRESS;
+    public static final int LONG_PACKET_MAX_RETRIES; /* how many times will payload with measurement data be sent after every minute */
+
     public static final String FILE_NAME; /* file used to persist incoming message data */
     public static final String FILE_EXTENSION;
 
-    private static boolean longPacketReceived;
+    /* this will all be stored in local config */
+    private static final long USER_ID;
+    private static final long INSTALLATION_ID;
+    private static final KeyPair KEY_PAIR;
+    private static final byte[] PUBLIC_KEY;
+
+    private static boolean longPacketReceived = false;
     private static Path tempFile;
 
-	static {
-		WORKER_THREADS = 1;
-		DEFAULT_CLIENT_PORT = 4501;
-		DEFAULT_SERVER_PORT = 4500;
-		LONG_PACKET_MAX_RETRIES = 5;
-		FILE_NAME = "tempfile";
+    static {
+        WORKER_THREADS = 1;
+        DEFAULT_CLIENT_PORT = 4501;
+        DEFAULT_SERVER_PORT = 4500;
+        LONG_PACKET_MAX_RETRIES = 5;
+
+        FILE_NAME = "tempfile";
         FILE_EXTENSION = ".tix";
-		try {
-			DEFAULT_SERVER_ADDRESS = new InetSocketAddress(InetAddress.getLocalHost(), DEFAULT_SERVER_PORT);
-		} catch (UnknownHostException e) {
-			logger.catching(e);
-			logger.fatal("Could not initialize the default server address");
-			throw new Error();
-		}
-	}
 
-	public static void main(String[] args) {
-		new TixTimeClient(DEFAULT_SERVER_ADDRESS, DEFAULT_CLIENT_PORT);
-	}
+        USER_ID = 11;
+        INSTALLATION_ID = 1;
+        KEY_PAIR = TixCoreUtils.NEW_KEY_PAIR.get();
+        PUBLIC_KEY = KEY_PAIR.getPublic().getEncoded();
+        try {
+            DEFAULT_SERVER_ADDRESS = new InetSocketAddress(InetAddress.getLocalHost(), DEFAULT_SERVER_PORT);
+        } catch (UnknownHostException e) {
+            logger.catching(e);
+            logger.fatal("Could not initialize the default server address");
+            throw new Error();
+        }
+    }
 
-	private TixTimeClient(InetSocketAddress serverAddress, int clientPort) {
-		logger.info("Starting Client");
-		logger.info("Server Address: {}:{}", serverAddress.getHostName(), serverAddress.getPort());
+    public static void main(String[] args) {
+        new TixTimeClient(DEFAULT_SERVER_ADDRESS, DEFAULT_CLIENT_PORT);
+    }
 
-		EventLoopGroup workerGroup;
-		Class<? extends Channel> datagramChannelClass;
-		if (Epoll.isAvailable()) {
-			logger.info("epoll available");
-			workerGroup = new EpollEventLoopGroup(WORKER_THREADS);
-			datagramChannelClass = EpollDatagramChannel.class;
-		} else {
-			logger.info("epoll unavailable");
-			logger.warn("epoll unavailable performance may be reduced due to single thread scheme.");
-			workerGroup = new NioEventLoopGroup(WORKER_THREADS, Executors.privilegedThreadFactory());
-			datagramChannelClass = NioDatagramChannel.class;
-		}
+    private TixTimeClient(InetSocketAddress serverAddress, int clientPort) {
+        logger.info("Starting Client");
+        logger.info("Server Address: {}:{}", serverAddress.getHostName(), serverAddress.getPort());
 
-		try {
-			logger.info("Setting up");
-			InetSocketAddress clientAddress = getClientAddress(clientPort);
-			logger.info("My Address: {}:{}", clientAddress.getAddress(), clientAddress.getPort());
+        EventLoopGroup workerGroup;
+        Class<? extends Channel> datagramChannelClass;
+        if (Epoll.isAvailable()) {
+            logger.info("epoll available");
+            workerGroup = new EpollEventLoopGroup(WORKER_THREADS);
+            datagramChannelClass = EpollDatagramChannel.class;
+        } else {
+            logger.info("epoll unavailable");
+            logger.warn("epoll unavailable performance may be reduced due to single thread scheme.");
+            workerGroup = new NioEventLoopGroup(WORKER_THREADS, Executors.privilegedThreadFactory());
+            datagramChannelClass = NioDatagramChannel.class;
+        }
+
+        try {
+            logger.info("Setting up");
+            InetSocketAddress clientAddress = getClientAddress(clientPort);
+            logger.info("My Address: {}:{}", clientAddress.getAddress(), clientAddress.getPort());
 
             tempFile = Files.createTempFile(FILE_NAME, FILE_EXTENSION);
             tempFile.toFile().deleteOnExit();
 
-			Bootstrap b = new Bootstrap();
-			b.group(workerGroup)
-					.channel(datagramChannelClass)
-					.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-					.handler(new ChannelInitializer<DatagramChannel>() {
-						@Override
-						protected void initChannel(DatagramChannel ch)
-								throws Exception {
-							ch.pipeline().addLast(new TixMessageDecoder());
-							ch.pipeline().addLast(new TixUdpClientHandler(clientAddress, serverAddress));
-							ch.pipeline().addLast(new TixMessageEncoder());
-						}
-					});
-			if (Epoll.isAvailable()) {
-				b.option(EpollChannelOption.SO_REUSEPORT, true);
-			}
-			logger.info("Binding into port {}", clientAddress.getPort());
-			Channel channel = b.bind(clientAddress).sync().channel();
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup)
+                    .channel(datagramChannelClass)
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .handler(new ChannelInitializer<DatagramChannel>() {
+                        @Override
+                        protected void initChannel(DatagramChannel ch)
+                                throws Exception {
+                            ch.pipeline().addLast(new TixMessageDecoder());
+                            ch.pipeline().addLast(new TixUdpClientHandler(clientAddress, serverAddress));
+                            ch.pipeline().addLast(new TixMessageEncoder());
+                        }
+                    });
+            if (Epoll.isAvailable()) {
+                b.option(EpollChannelOption.SO_REUSEPORT, true);
+            }
+            logger.info("Binding into port {}", clientAddress.getPort());
+            Channel channel = b.bind(clientAddress).sync().channel();
 
-			writePackets(0,1000,clientAddress, serverAddress, channel, tempFile);
+            writePackets(0, 1000, clientAddress, serverAddress, channel, tempFile);
 
-			ChannelFuture future = channel.closeFuture().await();
-			if (!future.isSuccess()) {
-				logger.error("Error while transmitting");
-			}
-		} catch (InterruptedException e) {
-			logger.fatal("Interrupted", e);
-			logger.catching(Level.FATAL, e);
-		} catch (UnknownHostException e) {
-			logger.fatal("Cannot retrieve local host address", e);
-			logger.catching(Level.FATAL, e);
-		} catch (IOException e) {
+            ChannelFuture future = channel.closeFuture().await();
+            if (!future.isSuccess()) {
+                logger.error("Error while transmitting");
+            }
+        } catch (InterruptedException e) {
+            logger.fatal("Interrupted", e);
+            logger.catching(Level.FATAL, e);
+        } catch (UnknownHostException e) {
+            logger.fatal("Cannot retrieve local host address", e);
+            logger.catching(Level.FATAL, e);
+        } catch (IOException e) {
             logger.fatal("Cannot persist incoming message data", e);
             logger.catching(Level.FATAL, e);
         } finally {
-			logger.info("Shutting down");
-			timer.cancel();
-			workerGroup.shutdownGracefully();
-		}
-	}
+            logger.info("Shutting down");
+            timer.cancel();
+            workerGroup.shutdownGracefully();
+        }
+    }
 
-	private void writePackets(final int delay, final int period, final InetSocketAddress clientAddress, final InetSocketAddress serverAddress, final Channel channel, final Path tempFile){
-		timer.scheduleAtFixedRate(new TimerTask() {
+    private void writePackets(final int delay, final int period, final InetSocketAddress clientAddress, final InetSocketAddress serverAddress, final Channel channel, final Path tempFile) {
+        timer.scheduleAtFixedRate(new TimerTask() {
             int i = 0;
             TixPacket shortPacket;
             TixPacket longPacketWithData;
             byte[] mostRecentData;
             byte[] signature;
-            boolean longPacketReceived = false;
 
-			@Override
-			public void run() {
-				// sending short message every second, no matter what
-                shortPacket = new TixPacket(clientAddress, serverAddress, TixPacketType.SHORT,TixCoreUtils.NANOS_OF_DAY.get());
-				channel.write(shortPacket);
-				channel.flush();
+            @Override
+            public void run() {
+                // sending short message every second, no matter what
+                shortPacket = new TixPacket(clientAddress, serverAddress, TixPacketType.SHORT, TixCoreUtils.NANOS_OF_DAY.get());
+                channel.write(shortPacket);
+                channel.flush();
 
-                if( i == 60 ){
+                if (i == 60) {
                     // send long packet once every minute, after short packet
                     try {
                         mostRecentData = Files.readAllBytes(tempFile);
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        logger.fatal("Could not read data from temp file", e);
+                        logger.catching(Level.FATAL, e);
                     }
-                    signature = TixCoreUtils.sign(mostRecentData,keyPair);
-                    longPacketWithData = new TixDataPacket(clientAddress, serverAddress, TixCoreUtils.NANOS_OF_DAY.get(), 11, 1, "mac".getBytes(), mostRecentData, signature);
+                    signature = TixCoreUtils.sign(mostRecentData, KEY_PAIR);
+                    longPacketWithData = new TixDataPacket(clientAddress, serverAddress, TixCoreUtils.NANOS_OF_DAY.get(), USER_ID, INSTALLATION_ID, PUBLIC_KEY, mostRecentData, signature);
                     channel.writeAndFlush(longPacketWithData);
                     i = 0;
                     try {
                         byte[] emptyByteArray = new byte[0];
-                        Files.write(tempFile,emptyByteArray, StandardOpenOption.TRUNCATE_EXISTING);
+                        Files.write(tempFile, emptyByteArray, StandardOpenOption.TRUNCATE_EXISTING);
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        logger.fatal("Could not empty contents of temp file", e);
+                        logger.catching(Level.FATAL, e);
                     }
                     longPacketReceived = false;
-                } else if ( i < LONG_PACKET_MAX_RETRIES ){
+                } else if (i < LONG_PACKET_MAX_RETRIES) {
                     // resend long packet if needed, a limited number of times
-                    if(longPacketReceived){
+                    if (longPacketReceived) {
                         mostRecentData = null;
                         signature = null;
                     } else {
-                        longPacketWithData = new TixDataPacket(clientAddress, serverAddress, TixCoreUtils.NANOS_OF_DAY.get(), 11, 1, "mac".getBytes(), mostRecentData, signature);
+                        longPacketWithData = new TixDataPacket(clientAddress, serverAddress, TixCoreUtils.NANOS_OF_DAY.get(), USER_ID, INSTALLATION_ID, PUBLIC_KEY, mostRecentData, signature);
                         channel.writeAndFlush(longPacketWithData);
                     }
-                } else if ( i == LONG_PACKET_MAX_RETRIES ){
+                } else if (i == LONG_PACKET_MAX_RETRIES) {
                     // long packet could not be sent, discard temporary copy
                     mostRecentData = null;
                     signature = null;
                 }
-				i++;
+                i++;
 
-			}
-		}, delay,period);
-	}
+            }
+        }, delay, period);
+    }
 
-	private InetSocketAddress getClientAddress(int clientPort) throws UnknownHostException {
-		return new InetSocketAddress(InetAddress.getLocalHost(), clientPort);
-	}
+    private InetSocketAddress getClientAddress(int clientPort) throws UnknownHostException {
+        return new InetSocketAddress(InetAddress.getLocalHost(), clientPort);
+    }
 
-	public static void setLongPacketReceived(boolean value){
+    public static void setLongPacketReceived(boolean value) {
         longPacketReceived = value;
     }
 
-    public static Path getTempFile(){
-	    return tempFile;
+    public static Path getTempFile() {
+        return tempFile;
     }
 
 }
--- src/main/java/com/github/tix_measurements/time/client/handler/TixUdpClientHandler.java
+++ src/main/java/com/github/tix_measurements/time/client/handler/TixUdpClientHandler.java
@@ -10,92 +10,72 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.time.LocalTime;
 
-import static com.github.tix_measurements.time.client.TixTimeClient.FILE_EXTENSION;
-import static com.github.tix_measurements.time.client.TixTimeClient.FILE_NAME;
 import static com.github.tix_measurements.time.client.TixTimeClient.getTempFile;
 
 public class TixUdpClientHandler extends ChannelInboundHandlerAdapter {
 
-	private final Logger logger = LogManager.getLogger();
-	private final InetSocketAddress fromAddress;
-	private final InetSocketAddress toAddress;
+    private final Logger logger = LogManager.getLogger();
+    private final InetSocketAddress fromAddress;
+    private final InetSocketAddress toAddress;
 
-	public TixUdpClientHandler(InetSocketAddress fromAddress, InetSocketAddress toAddress) {
-		this.fromAddress = fromAddress;
-		this.toAddress = toAddress;
-	}
+    public TixUdpClientHandler(InetSocketAddress fromAddress, InetSocketAddress toAddress) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+    }
 
-	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		logger.entry(ctx, msg);
-		TixPacket incomingMessage = (TixPacket)msg;
-		logger.info("Received package {} from {}", incomingMessage, incomingMessage.getFrom());
-		persistTixPacket(incomingMessage);
-		super.channelRead(ctx, msg);
-		logger.exit();
-	}
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        logger.entry(ctx, msg);
+        TixPacket incomingMessage = (TixPacket) msg;
+        logger.info("Received package {} from {}", incomingMessage, incomingMessage.getFrom());
+        persistTixPacket(incomingMessage);
+        super.channelRead(ctx, msg);
+        logger.exit();
+    }
 
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		logger.entry(ctx, cause);
-		logger.catching(cause);
-		logger.error("exception caught", cause);
-		logger.exit();
-	}
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.entry(ctx, cause);
+        logger.catching(cause);
+        logger.error("exception caught", cause);
+        logger.exit();
+    }
 
-	private void persistTixPacket(final TixPacket packet){
+    private void persistTixPacket(final TixPacket packet) {
         try {
-        	if(packet.getType() == TixPacketType.LONG){
+            if (packet.getType() == TixPacketType.LONG) {
 
                 TixTimeClient.setLongPacketReceived(true);
 
-			} else if(packet.getFinalTimestamp() != 0){
+            } else if (packet.getFinalTimestamp() != 0) {
 
-				final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				final ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-				final ByteBuffer longBuffer = ByteBuffer.allocate(Long.BYTES);
+                final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                final ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+                final ByteBuffer longBuffer = ByteBuffer.allocate(Long.BYTES);
 
-				final long unixTime = System.currentTimeMillis() / 1000L; // seconds passed since UNIX epoch
-				outputStream.write(longBuffer.putLong(unixTime).array());
+                final long unixTime = System.currentTimeMillis() / 1000L; // seconds passed since UNIX epoch
+                outputStream.write(longBuffer.putLong(unixTime).array());
 
-				outputStream.write(TixDataPacket.DATA_DELIMITER.getBytes());
+                outputStream.write(packet.getType() == TixPacketType.LONG ? (byte) 'L' : (byte) 'S'); //char to byte cast should be OK for ASCII chars
 
-				outputStream.write(packet.getType() == TixPacketType.LONG ? (byte)'L' : (byte)'S'); //char to byte cast should be OK for ASCII chars
+                outputStream.write(intBuffer.putInt(packet.getType().getSize()).array());
 
-				outputStream.write(TixDataPacket.DATA_DELIMITER.getBytes());
+                outputStream.write(longBuffer.putLong(packet.getInitialTimestamp()).array());
 
-				outputStream.write(intBuffer.putInt(packet.getType().getSize()).array());
+                outputStream.write(longBuffer.putLong(packet.getReceptionTimestamp()).array());
 
-				outputStream.write(TixDataPacket.DATA_DELIMITER.getBytes());
+                outputStream.write(longBuffer.putLong(packet.getSentTimestamp()).array());
 
-				outputStream.write(longBuffer.putLong(packet.getInitialTimestamp()).array());
+                outputStream.write(longBuffer.putLong(packet.getFinalTimestamp()).array());
 
-				outputStream.write(TixDataPacket.DATA_DELIMITER.getBytes());
-
-				outputStream.write(longBuffer.putLong(packet.getReceptionTimestamp()).array());
-
-				outputStream.write(TixDataPacket.DATA_DELIMITER.getBytes());
-
-				outputStream.write(longBuffer.putLong(packet.getSentTimestamp()).array());
-
-				outputStream.write(TixDataPacket.DATA_DELIMITER.getBytes());
-
-				outputStream.write(longBuffer.putLong(packet.getFinalTimestamp()).array());
-
-				outputStream.write("\r\n".getBytes());
-
-				Files.write(getTempFile(), outputStream.toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE, StandardOpenOption.SYNC);
-			}
+                Files.write(getTempFile(), outputStream.toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE, StandardOpenOption.SYNC);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
